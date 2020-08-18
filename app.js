ebda20e011b7fa5962ca214d67e214f52145320e
hanandito <hananditos@gmail.com>
2020-08-18 20:35:27 +0700

    initial commit

---
+++ app.js
@@ -0,0 +1,7 @@
+
+     $('.nav-vexpo .menu-icon').click(function() {
+      $(".side-menu").addClass("active");
+    })
+    $('.side-menu .fa-times, .side-menu li').click(function() {
+      $(".side-menu").removeClass("active");
+    })
